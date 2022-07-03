fa0eb975c835bed1775b87d566089b46d76a1684
anubrata07 <anubrata2001.choudhury@gmail.com>
2022-07-03 12:53:00 +0530

    CurrencyConverter changes

--- app/src/main/java/com/example/currencyconvertapp/MainActivity.java
+++ app/src/main/java/com/example/currencyconvertapp/MainActivity.java
@@ -14,7 +14,7 @@ import java.util.Formatter;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button euro, dollar, pound, yen, dinar, bitcoin, rubel, aus, can;
+    Button euro, dollar, pound, yen, dinar, bitcoin, ruble, aus, can;
     EditText editText;
     TextView textView;
 
@@ -29,9 +29,12 @@ public class MainActivity extends AppCompatActivity {
         yen=findViewById(R.id.yen);
         dinar=findViewById(R.id.dinar);
         bitcoin=findViewById(R.id.bitcoin);
-        rubel=findViewById(R.id.rubel);
+        ruble=findViewById(R.id.ruble);
         aus=findViewById(R.id.aus);
         can=findViewById(R.id.can);
+
+        editText=findViewById(R.id.editText);
+        textView=findViewById(R.id.textView);
 
         euro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,6 +59,200 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        dollar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String z=editText.getText().toString();
+
+                if(TextUtils.isEmpty(z)){
+                    editText.setError("Empty User Input");
+                }else{
+                    double n,k;
+
+                    n=Double.parseDouble(z);
+                    textView.setText(null);
+
+                    Formatter formatter=new Formatter();
+
+                    k=n * 0.013;
+                    DecimalFormat numberFormat= new DecimalFormat("#.00");
+                    textView.setText(""+numberFormat.format(k));
+
+//                    textView.setText(""+k);
+                }
+            }
+        });
+
+        pound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String z=editText.getText().toString();
+
+                if(TextUtils.isEmpty(z)){
+                    editText.setError("Empty User Input");
+                }else{
+                    double n,k;
+
+                    n=Double.parseDouble(z);
+                    textView.setText(null);
+
+                    Formatter formatter=new Formatter();
+
+                    k=n * 0.010;
+                    DecimalFormat numberFormat= new DecimalFormat("#.00");
+                    textView.setText(""+numberFormat.format(k));
+
+//                    textView.setText(""+k);
+                }
+            }
+        });
+
+        yen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String z=editText.getText().toString();
+
+                if(TextUtils.isEmpty(z)){
+                    editText.setError("Empty User Input");
+                }else{
+                    double n,k;
+
+                    n=Double.parseDouble(z);
+                    textView.setText(null);
+
+                    Formatter formatter=new Formatter();
+
+                    k=n * 1.17;
+                    DecimalFormat numberFormat= new DecimalFormat("#.00");
+                    textView.setText(""+numberFormat.format(k));
+
+//                    textView.setText(""+k);
+                }
+            }
+        });
+
+        dinar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String z=editText.getText().toString();
+
+                if(TextUtils.isEmpty(z)){
+                    editText.setError("Empty User Input");
+                }else{
+                    double n,k;
+
+                    n=Double.parseDouble(z);
+                    textView.setText(null);
+
+                    Formatter formatter=new Formatter();
+
+                    k=n * 0.047;
+                    DecimalFormat numberFormat= new DecimalFormat("#.00");
+                    textView.setText(""+numberFormat.format(k));
+
+//                    textView.setText(""+k);
+                }
+            }
+        });
+
+        bitcoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String z=editText.getText().toString();
+
+                if(TextUtils.isEmpty(z)){
+                    editText.setError("Empty User Input");
+                }else{
+                    double n,k;
+
+                    n=Double.parseDouble(z);
+                    textView.setText(null);
+
+                    Formatter formatter=new Formatter();
+
+                    k=n * 0.0000015;
+                    DecimalFormat numberFormat= new DecimalFormat("#.00");
+                    textView.setText(""+numberFormat.format(k));
+
+//                    textView.setText(""+k);
+                }
+            }
+        });
+
+        ruble.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String z=editText.getText().toString();
+
+                if(TextUtils.isEmpty(z)){
+                    editText.setError("Empty User Input");
+                }else{
+                    double n,k;
+
+                    n=Double.parseDouble(z);
+                    textView.setText(null);
+
+                    Formatter formatter=new Formatter();
+
+                    k=n * 0.72;
+                    DecimalFormat numberFormat= new DecimalFormat("#.00");
+                    textView.setText(""+numberFormat.format(k));
+
+//                    textView.setText(""+k);
+                }
+            }
+        });
+
+        aus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String z=editText.getText().toString();
+
+                if(TextUtils.isEmpty(z)){
+                    editText.setError("Empty User Input");
+                }else{
+                    double n,k;
+
+                    n=Double.parseDouble(z);
+                    textView.setText(null);
+
+                    Formatter formatter=new Formatter();
+
+                    k=n * 0.019;
+                    DecimalFormat numberFormat= new DecimalFormat("#.00");
+                    textView.setText(""+numberFormat.format(k));
+
+//                    textView.setText(""+k);
+                }
+            }
+        });
+
+        can.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String z=editText.getText().toString();
+
+                if(TextUtils.isEmpty(z)){
+                    editText.setError("Empty User Input");
+                }else{
+                    double n,k;
+
+                    n=Double.parseDouble(z);
+                    textView.setText(null);
+
+                    Formatter formatter=new Formatter();
+
+                    k=n * 0.016;
+                    DecimalFormat numberFormat= new DecimalFormat("#.00");
+                    textView.setText(""+numberFormat.format(k));
+
+//                    textView.setText(""+k);
+                }
+            }
+        });
+
+
 
 
 
